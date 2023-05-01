1e985e2227e4c4bd094bcee884cc8d814cf00cc8
YolandaAlvarez <yolandaa.alvarez.perez@gmail.com>
2023-05-01 15:53:54 -0600

    commit

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import url from '../.env.local';
 
 export default axios.create({
     //baseURL:'http://localhost:8081',
-    baseURL:'http://recipes-service.us-west-1.elasticbeanstalk.com/',
+    baseURL: process.env.SERVER_URL,
     headers: {"ngrok-skip-browser-warning": "true"}
 });
